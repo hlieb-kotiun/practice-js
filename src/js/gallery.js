442afb24522b815bd89eed2b16a29c46a13be5b1
Olena_Chovgun <olenkachovgun@gmail.com>
2024-12-08 15:53:58 +0200

    pagination

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,29 @@
 import UnsplashAPI from './UnsplashAPI';
-import renderImages from './renderfunction'
+import renderImages from './renderfunction';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector('.gallery');
 const api = new UnsplashAPI();
+const container = document.getElementById('tui-pagination-container');
+const pagination = new Pagination(container, {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+});
 
-api.getPopularPhotos(1).then(data => {
+const page = pagination.getCurrentPage();
+
+api.getPopularPhotos(page).then(data => {
   console.log(data);
-  gallery.innerHTML = renderImages(data.results)
+  gallery.innerHTML = renderImages(data.results);
+  pagination.reset(data.total);
+});
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  api
+    .getPopularPhotos(currentPage)
+    .then(data => (gallery.innerHTML = renderImages(data.results)));
 });
