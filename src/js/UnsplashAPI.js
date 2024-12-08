9844185c0f307adfec41b2af1546317462f5fa27
yuliia_garreau <yuliia.garreau@gmail.com>
2024-12-08 14:31:43 +0000

    save

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = "";
 
   async getPopularPhotos(page) {
     const params = new URLSearchParams({
@@ -14,5 +15,21 @@ export default class UnsplashAPI {
     });
     const { data } = await axios(`${this.#BASE_URL}?${params}`);
     return data;
+  }
+
+  async getPhotos(page) {
+    const params = new URLSearchParams({
+      query: this.#query,
+      page,
+      per_page: 12,
+      orientation: 'portrait',
+      client_id: this.#API_KEY,
+    });
+    const { data } = await axios(`${this.#BASE_URL}?${params}`);
+    return data;
+  } 
+
+  set query(newQuery) {
+this.#query = newQuery;
   }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,7 +1,10 @@
+import iziToast from 'izitoast';
 import UnsplashAPI from './UnsplashAPI';
 import renderImages from './renderfunction';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
+import error from "../img/javascript.svg";
+import "izitoast/dist/css/iziToast.min.css"; 
 
 const gallery = document.querySelector('.gallery');
 const api = new UnsplashAPI();
@@ -21,9 +24,49 @@ api.getPopularPhotos(page).then(data => {
   pagination.reset(data.total);
 });
 
-pagination.on('afterMove', event => {
-  const currentPage = event.page;
-  api
-    .getPopularPhotos(currentPage)
-    .then(data => (gallery.innerHTML = renderImages(data.results)));
-});
+pagination.on('afterMove', handlePagination);
+
+function handlePagination(event) {
+      const currentPage = event.page;
+    api
+      .getPopularPhotos(currentPage)
+      .then(data => (gallery.innerHTML = renderImages(data.results)));
+  }
+
+  function handleSubmitPagination(event) {
+    const currentPage = event.page;
+    api
+      .getPhotos(currentPage)
+      .then(data => (gallery.innerHTML = renderImages(data.results)));
+   
+  }
+
+const form = document.querySelector(".search-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+
+  event.preventDefault();
+  const inputValue = form.elements.query.value.trim();
+  if (inputValue === "") {
+    iziToast.warning({
+      iconUrl: error,
+      title: 'Caution',
+      message: 'The field is empty, please type your request',
+  })
+return;  
+}
+
+pagination.off('afterMove', handlePagination);
+pagination.off('afterMove', handleSubmitPagination);
+
+api
+.query = inputValue;
+api.getPhotos(page)
+.then(data => {
+  gallery.innerHTML = renderImages(data.results);
+  pagination.reset(data.total);
+})
+
+pagination.on('afterMove', handleSubmitPagination);
+
+}
