64a344e3ff97d4cc32eecc9caf2bd9e195cab6f3
Olena_Chovgun <olenkachovgun@gmail.com>
2024-12-08 16:57:45 +0200

    add final

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,8 +3,8 @@ import UnsplashAPI from './UnsplashAPI';
 import renderImages from './renderfunction';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
-import error from "../img/javascript.svg";
-import "izitoast/dist/css/iziToast.min.css"; 
+import error from '../img/javascript.svg';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const gallery = document.querySelector('.gallery');
 const api = new UnsplashAPI();
@@ -17,56 +17,75 @@ const pagination = new Pagination(container, {
 });
 
 const page = pagination.getCurrentPage();
+const loader = document.querySelector('.loader');
 
-api.getPopularPhotos(page).then(data => {
-  console.log(data);
-  gallery.innerHTML = renderImages(data.results);
-  pagination.reset(data.total);
-});
+api
+  .getPopularPhotos(page)
+  .then(data => {
+    console.log(data);
+    gallery.innerHTML = renderImages(data.results);
+    pagination.reset(data.total);
+  })
+  .catch(error => iziToast.error({ message: error.message || 'Error' }))
+  .finally(() => loader.classList.add('hidden'));
 
 pagination.on('afterMove', handlePagination);
 
 function handlePagination(event) {
-      const currentPage = event.page;
-    api
-      .getPopularPhotos(currentPage)
-      .then(data => (gallery.innerHTML = renderImages(data.results)));
-  }
+  const currentPage = event.page;
+  api
+    .getPopularPhotos(currentPage)
+    .then(data => (gallery.innerHTML = renderImages(data.results)));
+}
 
-  function handleSubmitPagination(event) {
-    const currentPage = event.page;
-    api
-      .getPhotos(currentPage)
-      .then(data => (gallery.innerHTML = renderImages(data.results)));
-   
-  }
+function handleSubmitPagination(event) {
+  const currentPage = event.page;
+  api
+    .getPhotos(currentPage)
+    .then(data => (gallery.innerHTML = renderImages(data.results)));
+}
 
-const form = document.querySelector(".search-form");
-form.addEventListener("submit", handleSubmit);
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
-
   event.preventDefault();
   const inputValue = form.elements.query.value.trim();
-  if (inputValue === "") {
+  if (inputValue === '') {
     iziToast.warning({
       iconUrl: error,
       title: 'Caution',
       message: 'The field is empty, please type your request',
-  })
-return;  
-}
+    });
+    return;
+  }
 
-pagination.off('afterMove', handlePagination);
-pagination.off('afterMove', handleSubmitPagination);
+  pagination.off('afterMove', handlePagination);
+  pagination.off('afterMove', handleSubmitPagination);
 
-api
-.query = inputValue;
-api.getPhotos(page)
-.then(data => {
-  gallery.innerHTML = renderImages(data.results);
-  pagination.reset(data.total);
-})
+  api.query = inputValue;
+  loader.classList.remove('hidden');
+  api
+    .getPhotos(page)
+    .then(data => {
+      if (data.results.length === 0) {
+        iziToast.warning({
+          iconUrl: error,
+          title: 'Caution',
+          message: 'Images not found',
+        });
 
-pagination.on('afterMove', handleSubmitPagination);
+        return;
+      }
+      if (data.total <= 12) {
+        container.classList.add('hidden');
+      } else {
+        container.classList.remove('hidden');
+      }
+      gallery.innerHTML = renderImages(data.results);
+      pagination.reset(data.total);
+    })
+    .catch(error => iziToast.error({ message: error.message || 'Error' }))
+    .finally(() => loader.classList.add('hidden'));
 
+  pagination.on('afterMove', handleSubmitPagination);
 }
